210d0503f7c6bc73ecbb49afa34b55ff76ee05c6
nnT_NJe0n9 <33746392+nnT-Nje0n9@users.noreply.github.com>
2020-05-06 02:27:36 +0900

    Add files via upload

---
+++ frequency analyzer.py
@@ -0,0 +1,24 @@
+#Frequency Analyzer
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+dic = {}
+cipher = input("Input The Cipher to analyze : ")
+
+for i in alphabet:
+    dic[i] = cipher.count(i)
+    print(i, " : ", cipher.count(i))
+
+for i in list(dic.keys()):
+    if dic[i] == 0:
+        del dic[i]
+
+dic = dict(sorted(dic.items(), key=lambda x:x[1], reverse=False))
+
+x = list(dic.keys())
+y = list(dic.values())
+
+plt.barh(x,y)
+plt.show()
